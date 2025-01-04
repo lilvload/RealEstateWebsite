e0eec003d4c58ecd697bd7932b32eee40ebf1945
lilvload <84636676+lilvload@users.noreply.github.com>
2025-01-04 12:04:39 +0200

    Delete RealtorAPI directory

--- RealtorAPI/RealtorAPI/Models/ApartmentDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RealtorAPI.Models;
-
-public class ApartmentDTO
-{
-    public string City { get; set; }
-    public string Address { get; set; }
-    public int Beds { get; set; }
-    public double Price { get; set; }
-}
